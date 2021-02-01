cc90af1ef54427e4b45b377d87330451bd09cd98
SeleneXX <761032438@qq.com>
2021-02-01 22:40:45 +0800

    test

--- djangolearning/settings.py
+++ djangolearning/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ix$g4l01l3%fze=1l@^m2gsop-*yi+2(5)0$+ezcbih_h+y1mb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -105,15 +105,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
